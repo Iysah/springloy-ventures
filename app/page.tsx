529fd2072296aa105f0f9f3876df9ad79fc63284
Yusuf Iysah  | UX Engineer <88023259+Iysah@users.noreply.github.com>
2024-10-14 04:02:30 +0100

    fixed deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
 import { Code, Globe, Smartphone, Zap } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
